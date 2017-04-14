ca051b63f52b49ea8ee6c72bf7c7e21118003dd2
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2017-04-14 10:25:32 -0500

    Some code cleanup and doc..

--- src/Dns.h
+++ src/Dns.h
@@ -1,32 +1,47 @@
-// Arduino DNS client for WizNet5100-based Ethernet shield
-// (c) Copyright 2009-2010 MCQN Ltd.
-// Released under Apache License, version 2.0
+/**
+ * Dns.h
+ *
+ * Arduino DNS client for WizNet5100-based Ethernet shield
+ *
+ * Portions (c) Copyright 2017 Gerad Munsch <gmunsch@unforgivendevelopment.com
+ * (c) Copyright 2009-2010 MCQN Ltd.
+ *
+ * Released under Apache License, version 2.0
+ *
+ */
 
-#ifndef DNSClient_h
-#define DNSClient_h
+#ifndef __DNSCLIENT_H__
+#define __DNSCLIENT_H__
 
 #include <EthernetUdp.h>
 
-class DNSClient
-{
+class DNSClient {
 public:
-    // ctor
+    /* ctor */
     void begin(const IPAddress& aDNSServer);
 
-    /** Convert a numeric IP address string into a four-byte IP address.
-        @param aIPAddrString IP address to convert
-        @param aResult IPAddress structure to store the returned IP address
-        @result 1 if aIPAddrString was successfully converted to an IP address,
-                else error code
-    */
+    /**
+     * Convert a numeric IP address string into a four-byte IP address.
+     *
+     * @brief Convert IP string into 4-byte IP.
+     * 
+     * @param[in] aIPAddrString IP address to convert
+     * @param[out] aResult IPAddress structure to store the returned IP address
+     * 
+     * @result 1 if aIPAddrString was successfully converted to an IP address, else error code
+     */
     int inet_aton(const char *aIPAddrString, IPAddress& aResult);
 
-    /** Resolve the given hostname to an IP address.
-        @param aHostname Name to be resolved
-        @param aResult IPAddress structure to store the returned IP address
-        @result 1 if aIPAddrString was successfully converted to an IP address,
-                else error code
-    */
+    /**
+     * Resolve the given hostname to an IP address.
+     *
+     * @brief Perform DNS resolution.
+     *
+     * @param[in] aHostname Name to be resolved
+     * @param[out] aResult IPAddress structure to store the returned IP address
+     *
+     * @result 1 if aIPAddrString was successfully converted to an IP address, else error code
+     */
     int getHostByName(const char* aHostname, IPAddress& aResult);
 
 protected:
@@ -38,4 +53,4 @@ protected:
     EthernetUDP iUdp;
 };
 
-#endif
+#endif  /* __DNSCLIENT_H__ */
